f82ef4b5a8f307ae7975a7af20930e1d6d723bd0
Kozmin Yurii <koriolan2@gmail.com>
2022-06-04 14:15:33 +0300

    Example fetch()

--- script.js
+++ script.js
@@ -3,50 +3,56 @@
 let url = 'https://jsonplaceholder.typicode.com/posts';
 
 const sendPost = () => {
-     return new Promise((resolve, reject)=>{
-        const request = new XMLHttpRequest();
-        request.addEventListener('readystatechange', ()=> {
-                    if (request.readyState !== 4) {return;}
-                    if (request.status === 200) {
-                        let data = request.responseText;
-                        resolve(data);
-                    } else {
-                        reject('Беда-печаль! Ошибка получилася!');
-                    }
-                });    
-        request.open('GET', url);
-        request.setRequestHeader('Content-type', 'application/json');
-        request.send();
-     });
-
-    
+    fetch(url, {
+                method: 'GET',
+                mode: 'cors',
+                })
+        .then((response) => {
+            if (response.status !== 200){
+                throw new Error('Reaspose status is not 200');
+            }
+            return response.json();
+        })       
+        .then ((data) => {
+            createWrap(data);
+        })
+        .catch((error) => console.error(error)) ;
 }
 
-sendPost()
-    .then((data)=>{return JSON.parse(data)})
-    .then((data)=> {createWrap(data)})
-    .catch((error)=>{console.error(error)})
-    .finally(() => console.log('Все сделано'));
-
-console.dir(Promise)
-
-
+sendPost();
 
 
 
 // const sendPost = () => {
-//     const request = new XMLHttpRequest();
-//     request.addEventListener('readystatechange', ()=> {
-//         if (request.readyState !== 4) {return;}
-//         if (request.status === 200) {
-//             let data = JSON.parse(request.responseText);
-//             createWrap(data);
-//         }
-//     });    
-//     request.open('GET', url);
-//     request.setRequestHeader('Content-type', 'application/json');
-//     request.send();
+//      return new Promise((resolve, reject)=>{
+//         const request = new XMLHttpRequest();
+//         request.addEventListener('readystatechange', ()=> {
+//                     if (request.readyState !== 4) {return;}
+//                     if (request.status === 200) {
+//                         let data = request.responseText;
+//                         resolve(data);
+//                     } else {
+//                         reject('Беда-печаль! Ошибка получилася!');
+//                     }
+//                 });    
+//         request.open('GET', url);
+//         request.setRequestHeader('Content-type', 'application/json');
+//         request.send();
+//      });
+
+    
 // }
+
+// sendPost()
+//     .then((data)=>{return JSON.parse(data)})
+//     .then((data)=> {createWrap(data)})
+//     .catch((error)=>{console.error(error)})
+//     .finally(() => console.log('Все сделано'));
+
+
+
+
+
 
 
 const createWrap = (data) => {
@@ -62,5 +68,3 @@ const createWrap = (data) => {
     }
     });
 }
-
-// sendPost();
